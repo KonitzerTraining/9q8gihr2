4d8dd979a1b440542eafa64b515a5638abc13654
M. Konitzer <mk@konitzer-training.de>
2024-07-22 12:16:17 +0000

    Directives

--- 01_standalone/src/app/template-syntax/template-syntax.component.ts
+++ 01_standalone/src/app/template-syntax/template-syntax.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-syntax',
   standalone: true,
   imports: [
-
+    CommonModule,
   ],
   templateUrl: './template-syntax.component.html',
   styleUrl: './template-syntax.component.scss'
 })
 export class TemplateSyntaxComponent {
+  public statusFlag = false;
 
+  // explizit oder implizit public
+  public textElement: string = 'Das ist ein Text';
+  public valueNumber = 340059.9348;
+  public buttonDisabled = true;
+  public html = 'Das ist ein <strong>wichtiger</strong> Inhalt';
 }
