6388c9af01097459cdcfc6e666be0d8689f85875
M. Konitzer <mk@konitzer-training.de>
2024-07-23 13:34:51 +0000

    environments

--- crm/src/app/features/customer/services/customer.service.ts
+++ crm/src/app/features/customer/services/customer.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Customer } from '../model/customer';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+const url = environment.api + 'customers/';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +17,6 @@ export class CustomerService {
   ) { }
 
   getAll(): Observable<Customer[]> {
-    return this.http.get<Customer[]>('http://localhost:3002/customers');
+    return this.http.get<Customer[]>(url);
   }
 }
---
+++ crm/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    api: 'http://localhost:3002/' // nur weil wir keinen Live-Server
+    // api: 'api/v1/' // im Realfall
+};
