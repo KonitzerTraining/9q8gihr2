eee74934fa90e73f73308087be986dc72f90dd5c
M. Konitzer <mk@konitzer-training.de>
2024-07-23 15:00:56 +0000

    LoadingIndicator

---
+++ crm/src/app/core/components/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  templateUrl: './loading-indicator.component.html',
+  styleUrl: './loading-indicator.component.scss'
+})
+export class LoadingIndicatorComponent {
+  
+  @Input()
+  show: boolean = false;
+
+}
--- crm/src/app/core/core.module.ts
+++ crm/src/app/core/core.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorBoxComponent } from './components/error-box/error-box.component';
+import { LoadingIndicatorComponent } from './components/loading-indicator/loading-indicator.component';
 
 @NgModule({
   declarations: [
-    ErrorBoxComponent
+    ErrorBoxComponent,
+    LoadingIndicatorComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ErrorBoxComponent
+    ErrorBoxComponent,
+    LoadingIndicatorComponent
   ]
 })
 export class CoreModule { }
