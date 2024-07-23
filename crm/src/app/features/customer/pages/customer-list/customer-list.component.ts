cf1e284a70acc7fe2661fbbdb3e44353d1f120e9
M. Konitzer <mk@konitzer-training.de>
2024-07-23 11:14:49 +0000

    Interface

--- crm/src/app/features/customer/pages/customer-list/customer-list.component.ts
+++ crm/src/app/features/customer/pages/customer-list/customer-list.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import type { Customer } from '../../model/customer';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html'
 })
 export class CustomerListComponent {
-
+  public customers: Customer[] = [
+    {
+      id: 4,
+      name: 'Tim',
+      credit: 25
+    }
+  ];
 }
